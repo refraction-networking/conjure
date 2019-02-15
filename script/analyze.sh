88b476cfeec2e1f5b8faa69976b03c3474d5cfd4
Eric Wustrow <ewust@colorado.edu>
2019-02-15 02:28:49 -0700

    add up/download figs

--- script/analyze.sh
+++ script/analyze.sh
@@ -3,12 +3,12 @@
 #python gen-addrs.py 100000 | python count-v6-bits.py | sort -n > rand.pdf
 #cat rand.pdf | cdf > rand.cdf
 
-cat all-by-pkt.out | grep '^2001:48a8:' | sort | uniq | python count-v6-bits.py | sort -n > merit-observed.pdf
+cat ./data/all-by-pkt.out | grep '^2001:48a8:' | sort | uniq | python count-v6-bits.py | sort -n > merit-observed.pdf
 cat merit-observed.pdf | cdf > merit-observed.cdf
 
 #cat merit-observed.pdf | sort -n | uniq -c > merit-observed.counts
 
-cat all-by-pkt.out | sort | uniq | python count-v6-bits.py | sort -n > all-observed.pdf
+cat ./data/all-by-pkt.out | sort | uniq | python count-v6-bits.py | sort -n > all-observed.pdf
 
 cat all-observed.pdf | cdf > all-observed.cdf
 
