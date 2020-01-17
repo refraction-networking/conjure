f208a153debfa69c0d1b8ffac9e7dde83fca3a46
jmwample <jack.wampler@colorado.edu>
2020-01-17 15:16:25 -0500

    format was broken -- functional and fixed

--- src/logging.rs
+++ src/logging.rs
@@ -28,7 +28,7 @@ impl log::Log for SimpleLogger
              && !s.starts_with("ticking")) {
                 let t = time::now();
                 // unwrap relies on "%b %d, %Y %T" being a valid format string.
-                let t_s = time::strftime("%Y-%m-%d %H:%M:%S.%.6f %z", &t).unwrap();
+                let t_s = time::strftime("%Y-%m-%d %H:%M:%S.%f %z", &t).unwrap();
                 println!("{} (Core {}) {}: {}", t_s, self.lcore_id, record.level(), s);
             }
         }
