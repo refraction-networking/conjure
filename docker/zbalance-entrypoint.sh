c3b20da697b3affd45f26cb677e2e5f031ef4dfd
Roman Kovtunovych <r.kovtunovych@psiphon.ca>
2021-06-15 20:24:36 -0400

    Use Ubuntu PF_RING package instead of submodule. (#91)
    * Modified .dockerignore to improve effectiveness of docker caching
    * Changed Dockerfile to use ntop pfring package
    * Added a script to add ntop repository and install pfring and ZC on the host system (Debian and Ubuntu)
    * Update docker default phantom_subnets.toml file
    * Added some sanity checks into zbalance container
    	* Check if hugepages number is 512
    	* Check if ZC driver is loaded
    	* If check not passed show some hints what can be wrong

--- docker/zbalance-entrypoint.sh
+++ docker/zbalance-entrypoint.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 set -e
 
+
+
+while [ $(sysctl -b vm.nr_hugepages) -lt 512 ]
+do
+	echo 'Please set number of hugepages to at least 512.'
+	echo ''
+	echo 'To check current value run:'
+	echo '	sysctl vm.nr_hugepages'
+	echo 'OR'
+	echo '	cat /proc/sys/vm/nr_hugepages'
+	echo ''
+	echo 'To set number of hugepages run:'
+	echo '	sysctl -w vm.nr_hugepages=512'
+	echo ''
+	echo 'To make this setting persistent run:'
+	echo '	echo "vm.nr_hugepages=512" >> /etc/sysctl.conf'
+	echo ''
+	echo ''
+	echo 'Sleeping for 10 seconds'
+	sleep 10
+done
+
+while [ ! $(cat "/proc/net/pf_ring/dev/${CJ_IFACE}/info" | grep ZC) ]
+do
+	echo 'Is ZC network drivers loaded? For instructions visit https://www.ntop.org/guides/pf_ring/get_started/packages_installation.html'
+	echo ''
+	echo 'To check for ZC driver run:'
+	echo '	cat /proc/net/pf_ring/dev/'${CJ_IFACE}'/info'
+	echo 'You should see "Polling Mode: ZC/NAPI"'
+	echo ''
+	sleep 10; 
+done
+
 # TD_IFACE could be a CSV list of interfaces.
 # Pull them apart to ensure each gets zc: prefix
 ifcarg=""
@@ -20,4 +53,4 @@ do
     fi
 done
 echo "Setting up with params: -i $ifcarg -c ${CJ_CLUSTER_ID} -n ${CJ_CORECOUNT} -m ${ZBALANCE_HASH_MODE} -g ${CJ_COREBASE}"
-/opt/PF_RING/userland/examples_zc/zbalance_ipc -i $ifcarg -c ${CJ_CLUSTER_ID} -n ${CJ_CORECOUNT} -m ${ZBALANCE_HASH_MODE} -g ${CJ_COREBASE}
+zbalance_ipc -i $ifcarg -c ${CJ_CLUSTER_ID} -n ${CJ_CORECOUNT} -m ${ZBALANCE_HASH_MODE} -g ${CJ_COREBASE}
