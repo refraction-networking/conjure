c4636bee86463eb326053ef49cad4bc97eca9b30
Eric Wustrow <ewust@colorado.edu>
2022-10-27 17:31:16 -0400

    add how to run zmqsub

--- script/sub-registrations/run.sh
+++ script/sub-registrations/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python3 phantom-sub.py
+#PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python3 phantom-sub.py
+
+go build zmqsub.go
+PHANTOM_SUBNET_LOCATION=/opt/conjure/sysconfig/phantom_subnets.toml ./zmqsub
+
