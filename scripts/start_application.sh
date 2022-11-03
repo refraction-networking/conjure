58dafecc02bd61d3ffa9a87441c8ad509492781a
jmwample <jack.wampler@colorado.edu>
2022-11-03 12:35:13 -0600

    docker-compose files and start scripts

---
+++ scripts/start_application.sh
@@ -0,0 +1,20 @@
+#!/bin/hash
+
+# Run Conjure application process using configs in environment variables.
+
+if [ $(id -u) -ne 0 ]; then
+    echo "$0 must be run as sudo"
+    exit 1
+fi
+
+# load config. will access config in /var/lib/conjure for overrides
+source /opt/conjure/sysconfig/conjure.conf
+
+if [ ! -f $CJ_STATION_CONFIG ]; then
+    echo "Failed to open \$CJ_STATION_CONFIG=$CJ_STATION_CONFIG."
+    echo "You may want to set CJ_STATION_CONFIG in the conjure.conf file before running the script"
+    exit 1
+fi
+
+
+/opt/conjure/bin/application
