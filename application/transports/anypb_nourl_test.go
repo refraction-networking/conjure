fae81170a6bc9c7df005c91b47f711202b088578
Mingye Chen <mingyechen82@gmail.com>
2023-04-28 11:50:17 -0400

    Add tests

--- application/transports/anypb_nourl_test.go
+++ application/transports/anypb_nourl_test.go
@@ -1,7 +1,9 @@
 package transports_test
 
 import (
+	"math/rand"
 	"testing"
+	"unsafe"
 
 	"github.com/refraction-networking/conjure/application/transports"
 	pb "github.com/refraction-networking/gotapdance/protobuf"
@@ -20,4 +22,41 @@ func TestUnmarshall(t *testing.T) {
 	require.Nil(t, err)
 
 	require.True(t, dst.GetRandomizeDstPort())
+}
+
+func TestMissingTypeURL(t *testing.T) {
+	src, err := anypb.New(&pb.GenericTransportParams{RandomizeDstPort: proto.Bool(true)})
+	require.Nil(t, err)
+	src.TypeUrl = ""
+
+	dst := &pb.GenericTransportParams{}
+	err = anypb.UnmarshalTo(src, dst, proto.UnmarshalOptions{})
+	require.NotNil(t, err)
+}
+
+func TestWrongType(t *testing.T) {
+	src, err := anypb.New(&pb.ClientToStation{Padding: []byte{0, 1}})
+	require.Nil(t, err)
+
+	dst := &pb.GenericTransportParams{}
+	err = transports.UnmarshalAnypbTo(src, dst)
+	require.Nil(t, err)
+
+	require.False(t, dst.GetRandomizeDstPort())
+}
+
+func TestGarbage(t *testing.T) {
+	src, err := anypb.New(&pb.ClientToStation{Padding: []byte{0, 1}})
+	require.Nil(t, err)
+	ptr := unsafe.Pointer(src)
+
+	for i := 0; i < int(unsafe.Sizeof(*src)); i++ {
+		*(*int)(ptr) = rand.Int()
+	}
+
+	dst := &pb.GenericTransportParams{}
+	err = transports.UnmarshalAnypbTo(src, dst)
+	require.Nil(t, err)
+
+	require.False(t, dst.GetRandomizeDstPort())
 }
