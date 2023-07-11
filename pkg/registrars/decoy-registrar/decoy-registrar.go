77ef0d922b8b2c115303b745588d81dc0ef759bf
marshall <stmr@umich.edu>
2023-07-11 12:20:48 -0400

    registrars: move decoy-registrar

--- pkg/registrars/decoy-registrar/decoy-registrar.go
+++ pkg/registrars/decoy-registrar/decoy-registrar.go
@@ -1,12 +1,20 @@
-package registration
+package decoy
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"net"
+	"time"
 
 	pb "github.com/refraction-networking/conjure/proto"
 	"github.com/refraction-networking/gotapdance/tapdance"
 	"github.com/sirupsen/logrus"
+)
+
+// Copied from dns-registrar
+var (
+	ErrRegFailed = errors.New("registration failed")
 )
 
 type DialFunc func(ctx context.Context, network, addr string) (net.Conn, error)
@@ -70,6 +78,7 @@ func (r DecoyRegistrar) Register(cjSession *tapdance.ConjureSession, ctx context
 	for _, decoy := range decoys {
 		logger.Debugf("Sending Reg: %v, %v", decoy.GetHostname(), decoy.GetIpAddrStr())
 		//decoyAddr := decoy.GetIpAddrStr()
+		// r.Send()
 		go reg.Send(ctx, decoy, dialErrors)
 	}
 
@@ -104,4 +113,75 @@ func (r DecoyRegistrar) Register(cjSession *tapdance.ConjureSession, ctx context
 	sleepWithContext(ctx, toSleep)
 
 	return reg, nil
+}
+
+func (r DecoyRegistrar) Send(ctx context.Context, decoy *pb.TLSDecoySpec, dialErrors chan error) {
+	deadline, deadlineAlreadySet := ctx.Deadline()
+
+	if !deadlineAlreadySet {
+		deadline = time.Now().Add(tapdance.GetRandomDuration(tapdance.deadlineTCPtoDecoyMin, tapdance.deadlineTCPtoDecoyMax))
+	}
+
+	childCtx, childCancelFunc := context.WithDeadline(ctx, deadline)
+	defer childCancelFunc()
+
+	//[reference] TCP to decoy
+	tcpToDecoyStartTs := time.Now()
+
+	//[Note] decoy.GetIpAddrStr() will get only v4 addr if a decoy has both
+	dialConn, err := r.dialContex(childCtx, "tcp", decoy.GetIpAddrStr())
+
+	reg.setTCPToDecoy(tapdance.durationToU32ptrMs(time.Since(tcpToDecoyStartTs)))
+	if err != nil {
+		if opErr, ok := err.(*net.OpError); ok && opErr.Err.Error() == "connect: network is unreachable" {
+			dialError <- RegError{msg: err.Error(), code: Unreachable}
+			return
+		}
+		dialError <- err
+		return
+	}
+
+	//[reference] connection stats tracking
+	rtt := rttInt(uint32(time.Since(tcpToDecoyStartTs).Milliseconds()))
+	delay := getRandomDuration(1061*rtt*2, 1953*rtt*3) //[TODO]{priority:@sfrolov} why these values??
+	TLSDeadline := time.Now().Add(delay)
+
+	tlsToDecoyStartTs := time.Now()
+	tlsConn, err := reg.createTLSConn(dialConn, decoy.GetIpAddrStr(), decoy.GetHostname(), TLSDeadline)
+	if err != nil {
+		dialConn.Close()
+		msg := fmt.Sprintf("%v - %v createConn: %v", decoy.GetHostname(), decoy.GetIpAddrStr(), err.Error())
+		dialError <- RegError{msg: msg, code: TLSError}
+		return
+	}
+	reg.setTLSToDecoy(durationToU32ptrMs(time.Since(tlsToDecoyStartTs)))
+
+	//[reference] Create the HTTP request for the registration
+	httpRequest, err := reg.createRequest(tlsConn, decoy)
+	if err != nil {
+		msg := fmt.Sprintf("%v - %v createReq: %v", decoy.GetHostname(), decoy.GetIpAddrStr(), err.Error())
+		dialError <- RegError{msg: msg, code: TLSError}
+		return
+	}
+
+	//[reference] Write reg into conn
+	_, err = tlsConn.Write(httpRequest)
+	if err != nil {
+		// // This will not get printed because it is executed in a goroutine.
+		// Logger().Errorf("%v - %v Could not send Conjure registration request, error: %v", decoy.GetHostname(), decoy.GetIpAddrStr(), err.Error())
+		tlsConn.Close()
+		msg := fmt.Sprintf("%v - %v Write: %v", decoy.GetHostname(), decoy.GetIpAddrStr(), err.Error())
+		dialError <- RegError{msg: msg, code: TLSError}
+		return
+	}
+
+	dialError <- nil
+	readAndClose(dialConn, time.Second*15)
+}
+
+// Move to other file eventually?
+func GetRandomDuration(base, min, max int) time.Duration {
+	addon := getRandInt(min, max) / 1000 // why this min and max???
+	rtt := rttInt(reg.getTcpToDecoy())
+	return time.Millisecond * time.Duration(base+rtt*addon)
 }
