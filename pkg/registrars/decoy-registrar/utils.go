c33307b174bee9d6dac8ad746394200f18b39b70
marshall <stmr@umich.edu>
2023-08-02 11:58:58 -0400

    core version

--- pkg/registrars/decoy-registrar/utils.go
+++ pkg/registrars/decoy-registrar/utils.go
@@ -186,30 +186,6 @@ func readAndClose(c net.Conn, readDeadline time.Duration) {
 	c.Close()
 }
 
-// CurrentClientLibraryVersion returns the current client library version used
-// for feature compatibility support between client and server. Currently I
-// don't intend to connect this to the library tag version in any way.
-//
-// When adding new client versions comment out older versions and add new
-// version below with a description of the reason for the new version.
-func currentClientLibraryVersion() uint32 {
-	// Support for randomizing destination port for phantom connection
-	// https://github.com/refraction-networking/gotapdance/pull/108
-	return 3
-
-	// // Selection algorithm update - Oct 27, 2022 -- Phantom selection version rework again to use
-	// // hkdf for actual uniform distribution across phantom subnets.
-	// // https://github.com/refraction-networking/conjure/pull/145
-	// return 2
-
-	// // Initial inclusion of client version - added due to update in phantom
-	// // selection algorithm that is not backwards compatible to older clients.
-	// return 1
-
-	// // No client version indicates any client before this change.
-	// return 0
-}
-
 // Below are functions adapted from tapdance/conjure.go that originally had receiver of
 // type *ConjureReg. For now, we can pass the *ConjureSession to work with and avoid any
 // receiver, but eventually we may want to change the receiver type to *ConjureSession,
@@ -246,7 +222,7 @@ func generateClientToStation(cjSession *td.ConjureSession) (*pb.ClientToStation,
 	//[reference] Generate ClientToStation protobuf
 	// transition := pb.C2S_Transition_C2S_SESSION_INIT
 	currentGen := td.Assets().GetGeneration()
-	currentLibVer := currentClientLibraryVersion()
+	currentLibVer := core.CurrentClientLibraryVersion()
 	transport := cjSession.Transport.ID()
 
 	transportParams, err := getPbTransportParams(cjSession)
