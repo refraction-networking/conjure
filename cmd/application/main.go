0b4c945ff18470d2d13a6b1084ca08cb98a69978
Mingye Chen <mingyechen82@gmail.com>
2024-02-13 10:37:04 -0500

    Add pprof

--- cmd/application/main.go
+++ cmd/application/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"net"
+	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
@@ -32,6 +33,9 @@ var enabledTransports = map[pb.TransportType]cj.Transport{
 }
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 	var err error
 	var zmqAddress string
 	flag.StringVar(&zmqAddress, "zmq-address", "ipc://@zmq-proxy", "Address of ZMQ proxy")
