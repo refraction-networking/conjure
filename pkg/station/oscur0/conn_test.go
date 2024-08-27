ef298382007c2434967c682b75d840c25d3477a4
Mingye Chen <mingyechen82@gmail.com>
2024-08-27 11:54:47 -0600

    Add test

--- pkg/station/oscur0/conn_test.go
+++ pkg/station/oscur0/conn_test.go
@@ -1,17 +1,65 @@
 package oscur0
 
-var privkey = "203963feed62ddda89b98857940f09866ae840f42e8c90160e411a0029b87e60"
+import (
+	"bytes"
+	"net"
+	"testing"
+)
 
-// func TestConn(t *testing.T) {
-// 	makePipe := func() (c1, c2 net.Conn, stop func(), err error) {
-// 		privkeyBytes := hex.DecodeString(privkey)
-// 		server, client := net.Pipe()
-// 		s, err := Server(dtlsnet.PacketConnFromConn(server), server.RemoteAddr(), &Config{})
-// 		if err != nil {
-// 			t.Fatalf("error creating server: %v", err)
-// 		}
+var pubkey = []byte{11, 99, 186, 173, 127, 47, 75, 181, 181, 71, 197, 58, 220, 15, 187, 23, 152, 82, 145, 6, 7, 147, 94, 111, 75, 86, 57, 253, 152, 155, 17, 86}
+var privkey = []byte{32, 57, 99, 254, 237, 98, 221, 218, 137, 185, 136, 87, 148, 15, 9, 134, 106, 232, 64, 244, 46, 140, 144, 22, 14, 65, 26, 0, 41, 184, 126, 96}
 
-// 		c, err := (dtlsnet.PacketConnFromConn(client), )
-// 	}
+func TestConn(t *testing.T) {
 
-// }
+	addr := &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: 1234}
+	phantom := "1.2.3.4:1234"
+	send := []byte("hi")
+	resp := []byte("hey")
+
+	listener, err := Listen(addr, Config{PrivKey: privkey})
+	if err != nil {
+		t.Fatalf("error creatign listener: %v", err)
+	}
+
+	go func() {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Fatalf("error listening conn: %v", err)
+		}
+
+		readBuf := make([]byte, len(send))
+
+		if _, err := conn.Read(readBuf); err != nil {
+			t.Fatalf("error reading buffer: %v", err)
+		}
+
+		if !bytes.Equal(readBuf, send) {
+			t.Fatalf("sent bytes != received")
+		}
+
+		if _, err := conn.Write(resp); err != nil {
+			t.Fatalf("error writing resp: %v", err)
+		}
+
+	}()
+
+	conn, err := Dial(addr, Config{PubKey: pubkey, Phantom: phantom})
+	if err != nil {
+		t.Fatalf("error creating client conn: %v", err)
+	}
+
+	if _, err := conn.Write(send); err != nil {
+		t.Fatalf("error writing send: %v", err)
+	}
+
+	readBuf := make([]byte, len(resp))
+
+	if _, err := conn.Read(readBuf); err != nil {
+		t.Fatalf("error reading buffer: %v", err)
+	}
+
+	if !bytes.Equal(readBuf, resp) {
+		t.Fatalf("sent bytes != received")
+	}
+
+}
