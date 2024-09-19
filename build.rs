7b1d2cea57891da740d1d27def6c554fbef28f83
Mingye Chen <mingyechen82@gmail.com>
2024-09-19 16:41:14 -0600

    Fix tapdance lib linking issue

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 extern crate cc;
 
 fn main() {
-    cc::Build::new()
-        .file("libtapdance/tapdance.c")
-        .include("src")
-        .compile("libtapdance.a");
+    println!("cargo:rustc-link-lib=tapdance");
+    println!("cargo:rustc-link-search=libtapdance");
+    // cc::Build::new()
+    //     .file("libtapdance/tapdance.c")
+    //     .include("src")
+    //     .compile("libtapdance.a");
 }
