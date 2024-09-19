43ef867792aafe207fc75c7589edcb025cbcefc9
Mingye Chen <mingyechen82@gmail.com>
2024-09-19 17:11:40 -0600

    Rm unneed line

--- build.rs
+++ build.rs
@@ -4,6 +4,5 @@ fn main() {
     println!("cargo:rustc-link-lib=tapdance");
     println!("cargo:rustc-link-search=libtapdance");
     println!("cargo:rustc-link-lib=gmp");
-    println!("cargo:rustc-link-lib=ssl");
     println!("cargo:rustc-link-lib=crypto");
 }
