67687d7d02f6cda3a892bfbb746cbf22d005c2ef
Mingye Chen <mingyechen82@gmail.com>
2024-10-22 18:20:38 -0400

    Fix build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,19 @@
 extern crate cc;
 
 fn main() {
+    cc::Build::new()
+        .files(&[
+            "libtapdance/tapdance.c",
+            "libtapdance/ssl_api.c",
+            "libtapdance/elligator2.c",
+            "libtapdance/curve25519-donna-c64.c",
+            "libtapdance/loadkey.c",
+            "libtapdance/tapdance_rst_spoof.c",
+            "libtapdance/tapdance_rust_util.c",
+        ])
+        .include("src")
+        .compile("libtapdance.a");
+
     println!("cargo:rustc-link-lib=tapdance");
     println!("cargo:rustc-link-search=libtapdance");
     println!("cargo:rustc-link-lib=gmp");
